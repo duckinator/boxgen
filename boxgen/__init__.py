3221db2b84895a25fbca61191f28442cdad4ffe5
Ellen Marie Dash <me@duckie.co>
2018-07-17 09:20:44 -0400

    weeeeeee

--- boxgen/__init__.py
+++ boxgen/__init__.py
@@ -1,12 +1,44 @@
 #!/usr/bin/env python3
 
+from collections import namedtuple
 import inspect
 import svgwrite
 import sys
 
+GridItem = namedtuple('GridItem', ['offset', 'size'])
+
+class Grid:
+    def __init__(self, cols, rows, layout):
+        self.cols   = cols
+        self.rows   = rows
+        self.items = self._parse_layout(layout)
+
+    @property
+    def items_with_offsets(self):
+        y_offset = 0
+        for row in range(0, len(self.rows)):
+            x_offset = 0
+            for col in range(0, len(self.cols)):
+                item = self.items[row][col]
+                if item != "-":
+                    offset = (x_offset, y_offset)
+                    size = (self.cols[col], self.rows[row])
+                    yield GridItem(offset, size)
+                x_offset += self.cols[col]
+            y_offset += self.rows[row]
+
+    def _parse_layout(self, layout):
+        items = layout.strip().split()
+        chunks = list(self._chunk(items, len(self.cols)))
+        assert len(chunks) == len(self.rows)
+        return chunks
+
+    def _chunk(self, lst, chunk_size):
+        for i in range(0, len(lst), chunk_size):
+            yield lst[i:i + chunk_size]
+
 class Boxgen:
     def __init__(self, height, width, thickness):
-        # 72mm height, 47mm width, 10mm thickness.
         self.height     = int(height)
         self.width      = int(width)
         self.depth      = int(thickness)
@@ -16,67 +48,27 @@ class Boxgen:
 
     @classmethod
     def rect(self, size, offset, stroke="black"):
-        if size[0] == None:
-            return None
         return svgwrite.shapes.Rect(insert=offset, size=size, stroke=stroke,
                 fill="white")
-
-    @classmethod
-    def build_side(self, x_offset, parts):
-        shapes = []
-        y_offset = 0
-        for size in parts:
-            shapes.append(self.rect(size, (x_offset, y_offset)))
-            y_offset += size[1]
-
-        return filter(None.__ne__, shapes)
 
     def generate(self):
         svg = svgwrite.Drawing(self.get_file_path(), profile='tiny')
 
+        depth = self.depth
+        width = self.width
         height = self.height
-        width  = self.width
-        depth  = self.depth
 
-        tiny = (depth, depth)
-        flap = (width, depth)
-        side = (depth, height)
-        face = (width, height)
+        grid = Grid(cols=[depth, width, depth, width, depth],
+                    rows=[depth, depth, height, depth],
+                    layout=
+                    "-    flap -    -    -    "
+                    "tiny flap tiny -    -    "
+                    "side face side face side "
+                    "tiny flap tiny flap -    ")
 
-        tiny_off = (None, tiny[1])
+        for item in grid.items_with_offsets:
+            svg.add(self.rect(item.size, item.offset))
 
-        x_offset = 0
-
-        left_side_outer = self.build_side(x_offset,
-                (tiny_off, tiny, side, tiny))
-        x_offset += tiny[0]
-
-        back = self.build_side(x_offset,
-                (flap, flap, face, flap))
-        x_offset += flap[0]
-
-        right_side = self.build_side(x_offset,
-                (tiny_off, tiny, side, tiny))
-        x_offset += tiny[0]
-
-        front = self.build_side(x_offset,
-                (tiny_off, tiny_off, face, flap))
-        x_offset += face[0]
-
-        left_side_inner = self.build_side(x_offset,
-                (tiny_off, tiny_off, side, tiny_off))
-        x_offset += side[0]
-
-        shapes = [
-            *left_side_outer,
-            *back,
-            *right_side,
-            *front,
-            *left_side_inner,
-        ]
-
-        for shape in shapes:
-            svg.add(shape)
         return svg
 
     def save(self):
